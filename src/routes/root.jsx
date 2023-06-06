c9f746625aa58e1dce857dd54c612a03e247d1a5
kazaneza <kazanezagentil@gmail.com>
2023-06-07 00:04:24 +0100

    access and render the data

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,7 +1,9 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLoaderData } from "react-router-dom";
 import { getContacts } from "../contacts";
 
+
 export default function Root() {
+  const {contacts} = useLoaderData();
     return (
       <>
         <div id="sidebar">
@@ -30,14 +32,29 @@ export default function Root() {
             </form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <Link to={`contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-                <Link to={`contacts/2`}>Your Friend</Link>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map ((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ):(
+                        <i>No name</i>
+                      ) }{" "}
+                      {contact.favorite && <span>★</span>}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          
           </nav>
         </div>
         <div id="detail">
