a1d2a1fc255a28783c5edc076053654c8cf2ba62
kazaneza <kazanezagentil@gmail.com>
2023-06-07 00:22:49 +0100

    access and render the data

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,9 @@
-import { Outlet, Link, useLoaderData } from "react-router-dom";
+import { Outlet, Link, useLoaderData, } from "react-router-dom";
 import { getContacts } from "../contacts";
 
 
 export default function Root() {
-  const {contacts} = useLoaderData();
+  const { contacts } = useLoaderData();
     return (
       <>
         <div id="sidebar">
@@ -32,28 +32,29 @@ export default function Root() {
             </form>
           </div>
           <nav>
-            {contacts.length ? (
-              <ul>
-                {contacts.map ((contact) => (
-                  <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
-                      {contact.first || contact.last ? (
-                        <>
-                          {contact.first} {contact.last}
-                        </>
-                      ):(
-                        <i>No name</i>
-                      ) }{" "}
-                      {contact.favorite && <span>★</span>}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>
-                <i>No contacts</i>
-              </p>
-            )}
+            
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
           
           </nav>
         </div>
@@ -64,7 +65,7 @@ export default function Root() {
     );
   }
 
-  export async function loader (){
-    const contacts = await getContacts()
-    return {contacts};
+  export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
   }
