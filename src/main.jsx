b80328dcef14ee5f6fce2b4bc759b5a4178f20ad
kazaneza <kazanezagentil@gmail.com>
2023-06-07 00:22:00 +0100

    configure the loader on the route

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
-import Root from './routes/root';
+// import Root from './routes/root';
 import ErrorPage from './error-page';
 import Contact from './routes/contact';
-import Root, {Loader as rootLoader} from "./routes/root"
+import Root, { loader as rootLoader } from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
-    Loader: rootLoader,
+    loader: rootLoader,
     children: [
       {
         path: "contacts/:contactId",
