edd011d95ed05cfee704a774570239c2a0bd9b4c
kazaneza <kazanezagentil@gmail.com>
2023-06-07 00:49:33 +0100

    import and set the action on the route

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css'
 // import Root from './routes/root';
 import ErrorPage from './error-page';
 import Contact from './routes/contact';
-import Root, { loader as rootLoader } from "./routes/root";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +16,7 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
